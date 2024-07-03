2dc12aae75c76384a33a0a48e16ec4d019ffb2c4
Nikon-33 <89101717800@mail.ru>
2024-07-03 20:36:42 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,23 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    string = (len(string), string.upper(), string.lower())
+    return string
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for i in range(len(list_to_search)):
+        if list_to_search[i].lower() == string.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
+
